ef9e8d78f55ce4f539468e9b33f1ddd230902257
oleHxOleksuk <108345191+oleHxOleksuk@users.noreply.github.com>
2022-11-19 22:50:01 +0200

    qwetry

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,37 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    console.log(data.seconds);
+    localStorage.setItem('timeUp', data.seconds);
+  }, 1000)
+);
+
+player
+  .setCurrentTime(localStorage.getItem('timeUp'))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+const throttle = require('lodash.throttle');
+const refs = {
+  onInputData: document.querySelector('.feedback-form'),
+};
 
+refs.onInputData.addEventListener('input', throttle(handleInputData, 500));
+window.addEventListener('load', updateOutputOnload);
+refs.onInputData.addEventListener('submit', onSubmit);
+
+function handleInputData(event) {
+  event.preventDefault();
+  const message = refs.onInputData.elements.message.value;
+  const email = refs.onInputData.elements.email.value;
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify({ message, email })
+  );
+}
+
+function updateOutputOnload(event) {
+  event.preventDefault();
+  const OutputText = JSON.parse(
+    localStorage.getItem('feedback-form-state')
+  ) || {
+    email: '',
+    message: '',
+  };
+  const { email, message } = OutputText;
+  refs.onInputData.elements.email.value = email;
+  refs.onInputData.elements.message.value = message;
+}
+
+function onSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  console.log({ email: email.value, message: message.value });
+  localStorage.removeItem('feedback-form-state');
+  refs.onInputData.reset();
+}
